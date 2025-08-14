f3e4d79efb268388263bb62e882b4289b440487d
Kausar Meer <kausarmeer087@gmail.com>
2025-08-14 20:33:42 +0530

    Commit

---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+
+import Header from "../other/Header";
+import TaskListNumbers from "../other/TaskListNumbers";
+import TaskList from "../TaskList/TaskList";
+
+const EmployeeDashboard = (props) => {
+  return (
+    <div className="p-10 bg-[#1C1C1C] h-screen">
+      <Header changeUser={props.changeUser} data={props.data} />
+      <TaskListNumbers data={props.data} />
+      <TaskList data={props.data} />
+    </div>
+  );
+};
+
+export default EmployeeDashboard;
